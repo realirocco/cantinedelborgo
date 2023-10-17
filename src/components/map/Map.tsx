af1371dfd9c4a146d6b521afdbc5408688a1ff06
Rocco Reali <rocco.reali@immobiliare.it>
2023-10-17 22:58:12 +0200

    feat: add current location on map

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,43 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {MapLibreTileLayer} from './MapLibreTileLayer'
-import { DivIcon } from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L, { DivIcon, LatLng } from "leaflet";
+import { MapContainer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'
 import pois from '../../data/pois.json'
 import './Map.css'
 
 
+function LocalPosition(){
+    const [position, setPosition] = useState(new LatLng(0,0));
+    const map = useMapEvents({
+        locationfound(e) {
+            setPosition(e.latlng)
+        }
+    });
+    map.locate({
+        watch: true,
+        timeout: 1000,
+        enableHighAccuracy: true
+    })
+    
+    return(
+        <Marker position={position} icon={customIcon(0, "qui")} >
+            <Popup>Sei qui!</Popup>
+        </Marker>
+    )
+}
+
+function iconContent(text:number, type:string){
+    return "<i class="+type+">"+text+"</i>";
+}
+
+function customIcon(text:number, type:string){
+    return new DivIcon({
+        iconSize: L.point([30,30]),
+        className: "map-icon",
+        html: iconContent(text, type),
+    });
+}
+
 
 export default class Map extends React.Component {
-
-    iconContent(text:number, type:string){
-         return "<i class="+type+">"+text+"</i>";
-    }
-
-    customIcon(text:number, type:string){
-        return new DivIcon({
-            iconSize: L.point([30,30]),
-            className: "map-icon",
-            html: this.iconContent(text, type),
-        });
-    }
-
+    
     componentDidMount(){
         window.dispatchEvent(new Event('resize')); 
     }
-
+      
     render() {
             
         return (
-            <MapContainer center={[41.601378, 13.491210]} maxNativeZoom={18} minZoom={18} zoom={18} maxZoom={20} >
+            <MapContainer center={[41.601378, 13.491210]} minZoom={18} zoom={18} maxZoom={20} 
+                maxBounds={[[41.602628, 13.488575],[41.600278, 13.494099]]} maxBoundsViscosity={0.7}>
                 <MapLibreTileLayer
-                    attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/about" target="_blank">OpenStreetMap</a> contributors'
+                    attribution='&copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/about" target="_blank">OpenStreetMap</a> contributors'
                     url="https://api.maptiler.com/maps/c25fe359-74e8-45c6-8d2d-bf5da6a2b36b/style.json?key=SwF3JvuRnEwDx2MufjH5"
                 />
                 {pois.map(poi => (
                     <Marker
                         key={poi.id}
-                        icon={this.customIcon(poi.numero, poi.tipo)}
+                        icon={customIcon(poi.numero, poi.tipo)}
                         position={[
                             poi.latitudine,
                             poi.longitudine
@@ -48,6 +69,7 @@ export default class Map extends React.Component {
                         </Popup>
                     </Marker>
                  ))}
+                <LocalPosition/>
             </MapContainer>
         );
     }
