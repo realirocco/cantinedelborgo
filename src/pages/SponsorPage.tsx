5ab118986985c3b8cc5b19ea7808150ba00d377c
Rocco Reali <rocco.reali@immobiliare.it>
2023-10-22 00:10:17 +0200

    feat: sponsor modal

--- src/pages/SponsorPage.tsx
+++ src/pages/SponsorPage.tsx
@@ -1,6 +1,23 @@
-import { IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCol, IonContent, IonGrid, IonHeader, IonImg, IonModal, IonPage, IonRow, IonTitle, IonToolbar,IonCard, IonCardHeader, IonCardTitle, IonButtons, IonButton, IonIcon } from '@ionic/react';
 import sponsors from '../data/sponsor.json'
-const SponsorPage: React.FC = () => {
+import { useRef, useState } from 'react';
+import { closeOutline } from 'ionicons/icons';
+
+function SponsorPage(){
+  let modal = useRef<HTMLIonModalElement>(null);
+
+  let [sponsor, setSponsor] = useState({})
+  let [isOpen, setIsOpen] = useState(false)
+
+
+  function CardSponsor(props:any){
+    return(
+      <IonCard className='ion-text-center' onClick={(e) => {setSponsor(props.sponsor); setIsOpen(true)}} >
+        <img src={'asset/sposors/'+props.sponsor.image} alt={props.sponsor.name} />
+      </IonCard>
+    )
+  }
+  
   return (
     <IonPage>
       <IonHeader>
@@ -14,9 +31,8 @@ const SponsorPage: React.FC = () => {
             {sponsors
               .filter((s) => s.value > 100)
               .map( (sponsor) => (
-                <IonCol size="12">
-                  <img src={'asset/sposors/'+sponsor.image} alt={sponsor.name} />
-                  <p>{sponsor.name}</p>
+                <IonCol size="12" key={sponsor.id}>
+                  <CardSponsor sponsor={sponsor} />
                 </IonCol>
               ))}
           </IonRow>
@@ -24,9 +40,8 @@ const SponsorPage: React.FC = () => {
             {sponsors
               .filter((s) => s.value <= 100 && s.value > 50)
               .map( (sponsor) => (
-                <IonCol size="6">
-                  <img src={'asset/sposors/'+sponsor.image} alt={sponsor.name} />
-                  <p>{sponsor.name}</p>
+                <IonCol size="6" key={sponsor.id}>
+                  <CardSponsor sponsor={sponsor} />
                 </IonCol>
               ))}
           </IonRow>
@@ -34,13 +49,28 @@ const SponsorPage: React.FC = () => {
             {sponsors
               .filter((s) => s.value <= 50)
               .map( (sponsor) => (
-                <IonCol size="4">
-                  <img src={'asset/sposors/'+sponsor.image} alt={sponsor.name} />
-                  <p>{sponsor.name}</p>
+                <IonCol size="4" key={sponsor.id}>
+                  <CardSponsor sponsor={sponsor} />
                 </IonCol>
               ))}
           </IonRow>
         </IonGrid>
+        <IonModal ref={modal} isOpen={isOpen} onDidDismiss={(e) => setIsOpen(false)} initialBreakpoint={0.7} breakpoints={[0.6,0.9]} >
+          <IonHeader>
+            <IonToolbar>
+              <IonButtons slot="end">
+                <IonButton onClick={() => setIsOpen(false)}><IonIcon icon={closeOutline}/></IonButton>
+              </IonButtons>
+              <IonTitle>{sponsor.name}</IonTitle>
+            </IonToolbar>
+            <img src={'asset/sposors/'+sponsor.image} alt={sponsor.name} />
+            <ul>
+              {Object.entries(sponsor?.contacts || {})
+                .map((entry, idx) => (<li key={idx}><b>{entry[0]}</b>: {entry[1]}</li>))
+              }
+            </ul>
+          </IonHeader>
+        </IonModal>
       </IonContent>
     </IonPage>
   );
