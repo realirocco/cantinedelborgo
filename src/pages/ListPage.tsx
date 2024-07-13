f418f8f39b62e73d64933fac166b7c42694fd814
Rocco Reali <rocco.reali@immobiliare.it>
2024-07-13 16:42:49 +0200

    refactoring list page

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,22 +1,25 @@
 import React, { useRef, useState } from 'react';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonModal, IonButtons, IonButton, IonIcon, IonSearchbar } from '@ionic/react';
 import pois from '../data/pois'
-import Map from '../components/map/Map';
+import Map from '../components/map/EventMap';
 import { closeOutline } from 'ionicons/icons';
 import { useLocation } from 'react-router';
+import { Poi } from '../util/Poi';
+import './ListPage.css'
 
-function filterPoi(poi:any, filter:String){
-  let nome = poi.nome.toLowerCase()
-  let descrizione = poi.descrizione.toLowerCase()
-  return nome.indexOf(filter) > -1 || descrizione.indexOf(filter) > -1
+function filterPoi(poi:Poi, filter:string, type: string){
+  let title = poi.title?.toLowerCase() ?? ""
+  let description = poi.description?.toLowerCase() ?? ""
+  let icon = poi.icon;
+  return icon==type && (title.indexOf(filter) > -1 || description.indexOf(filter) > -1)
 }
 
-function highlight(text:string|undefined, search:string){
+function highlight(text:string|undefined|null, search:string){
   var highlight = text || "";
   if(search && text){
     var searchString = search.replace(/[^a-zA-Z0-9]/,search);
     var searchRegExp = new RegExp('(' + searchString + ')', "gi");
-    highlight = text.replace(searchRegExp, "<strong>$1</strong>");
+    highlight = text.replace(searchRegExp, "<b>$1</b>");
   }
   return <span dangerouslySetInnerHTML={{__html:highlight}}/>
 }
@@ -26,7 +29,8 @@ function ListPage(props:any){
   let modal = useRef<HTMLIonModalElement>(null);
   let searchbar = useRef<HTMLIonSearchbarElement>(null);
 
-  let [poi, setPoi] = useState({})
+  let voidPoi:Poi = {id:"0", title:"", icon:"", iconText:"", lat:0, lng:0, description:""};
+  let [poi, setPoi] = useState(voidPoi)
   let [isOpen, setIsOpen] = useState(false)
   let [searchValue, setSearchValue] = useState("")
 
@@ -48,16 +52,15 @@ function ListPage(props:any){
           </IonToolbar>
         </IonHeader>
           {pois
-            .filter(poi => poi.tipo === "cantina")
-            .filter(poi => filterPoi(poi, searchValue))              
+            .filter(poi => filterPoi(poi, searchValue, "cantina"))              
             .map(poi => (
               <IonCard key={poi.id} onClick={(e) => {setPoi(poi); setIsOpen(true)}}>
                 <IonCardHeader>
-                  <IonCardSubtitle>Cantina numero {poi.numero}</IonCardSubtitle>
-                  <IonCardTitle>{highlight(poi.nome,searchValue)}</IonCardTitle>
+                  <IonCardSubtitle>Cantina numero {poi.iconText}</IonCardSubtitle>
+                  <IonCardTitle className='highlight'>{highlight(poi.title,searchValue)}</IonCardTitle>
                 </IonCardHeader>
                 <IonCardContent>
-                  <p>{highlight(poi.descrizione,searchValue)}</p>
+                  <p  className='highlight'>{highlight(poi.description,searchValue)}</p>
                 </IonCardContent>
               </IonCard>
             ))};
@@ -68,15 +71,15 @@ function ListPage(props:any){
             <IonButtons slot="end">
               <IonButton onClick={() => setIsOpen(false)}><IonIcon icon={closeOutline}/></IonButton>
             </IonButtons>
-            <IonTitle size='small'>Cantina n.{poi.numero}</IonTitle>
-            <IonTitle size="large">{poi.nome}</IonTitle>
+            <IonTitle size='small'>Cantina {poi.iconText}</IonTitle>
+            <IonTitle size="large">{poi.title}</IonTitle>
           </IonToolbar>
           <IonToolbar>
-            <IonTitle size='small'>{poi.descrizione}</IonTitle>
+            <IonTitle size='small'>{poi.description}</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonContent>
-          <Map center={[poi.latitudine, poi.longitudine]} markers={[poi]}/>
+          <Map center={[poi.lat, poi.lng]} markers={[poi]}/>
         </IonContent>
       </IonModal>
     </IonPage>
