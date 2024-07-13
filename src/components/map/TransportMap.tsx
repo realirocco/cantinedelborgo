4939281e07fa67d8f026b30c2e05471835c3ae8c
Rocco Reali <rocco.reali@immobiliare.it>
2024-07-13 20:26:10 +0200

    use vector map on transport map

--- src/components/map/TransportMap.tsx
+++ src/components/map/TransportMap.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import L, { LatLngExpression } from "leaflet";
-import { MapContainer, Polyline, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css'
-import track from '../../data/track';
-import { Poi } from '../../util/Poi';
-import LocalPosition from '../localposition/LocalPosition';
 import Bus from './Bus'
+import LocalPosition from '../localposition/LocalPosition';
+import { MapLibreTileLayer } from './MapLibreTileLayer';
+import { LatLngExpression } from "leaflet";
+import { MapContainer, Polyline } from 'react-leaflet';
+import { Poi } from '../../util/Poi';
+import track from '../../data/track';
 import './Map.css'
+import 'leaflet/dist/leaflet.css'
 
 export default class TransportMap extends React.Component {
     
@@ -19,13 +20,10 @@ export default class TransportMap extends React.Component {
     render() {
         return (
             <MapContainer center={this.props.center} minZoom={14} zoom={18} maxZoom={18} maxBoundsViscosity={1} >
-                <TileLayer           
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Polyline positions={track} weight={10}/>
-                <LocalPosition/>
-                <Bus id="12"/>
+                <MapLibreTileLayer url="https://api.maptiler.com/maps/c25fe359-74e8-45c6-8d2d-bf5da6a2b36b/style.json?key=SwF3JvuRnEwDx2MufjH5" attribution='' />
+                <Polyline positions={track} weight={10} />
+                <LocalPosition />
+                <Bus id="12" />
             </MapContainer>
         );
     }
