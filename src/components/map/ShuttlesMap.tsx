8fe8d58599197c99d5297bbdde1f49f65eccd877
Rocco Reali <rocco.reali@immobiliare.it>
2024-10-28 21:55:45 +0100

    chore(shuttle): line weight to 8

--- src/components/map/ShuttlesMap.tsx
+++ src/components/map/ShuttlesMap.tsx
@@ -21,7 +21,7 @@ export default class TransportMap extends React.Component {
         return (
             <MapContainer center={this.props.center} minZoom={14} zoom={18} maxZoom={18} maxBoundsViscosity={1} >
                 <MapLibreTileLayer url="https://api.maptiler.com/maps/c25fe359-74e8-45c6-8d2d-bf5da6a2b36b/style.json?key=SwF3JvuRnEwDx2MufjH5" attribution='' />
-                <Polyline positions={track} weight={10} />
+                <Polyline positions={track} weight={8} />
                 <LocalPosition />
                 <Shuttle />
             </MapContainer>
